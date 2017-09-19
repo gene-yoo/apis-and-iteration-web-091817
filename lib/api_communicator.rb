50547b4a9853e225e4e816bd8d8be1daa156036b
gene-yoo <gene.y.yoo@gmail.com>
2017-09-19 14:37:51 -0400

    Add executable and helper methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,39 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+
+  while (character_hash.fetch("next"))
+    character_hash["results"].each do |character_data|
+      if character_data.fetch("name").downcase == character
+        return character_data.fetch("films").collect {|film| JSON.parse(RestClient.get(film))}
+      end
+    end
+    all_characters = RestClient.get(character_hash.fetch("next"))
+    character_hash = JSON.parse(all_characters)
+  end
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |film, index|
+    puts "#{index + 1} #{film["title"]}"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+
+  if films_hash
+    parse_character_movies(films_hash)
+  else
+    puts "No match found."
+  end
 end
 
 ## BONUS
 
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+def most_expensive_vehicle_i_can_afford(limit)
+
+end
